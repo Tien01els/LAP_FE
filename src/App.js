645c6d0940f1b7542a95f567033a6ef97b090f5f
Tien01els <tien140801@gmail.com>
2022-09-12 16:01:45 +0700

    LAP_FE

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Suspense } from "react";
+import AppRouter from "./AppRouter";
+
+function App() {
+  return (
+    <>
+      <Suspense fallback={<></>}>
+        <AppRouter></AppRouter>
+      </Suspense>
+    </>
+  );
+}
+
+export default App;
