2929bf9d77ec4fc5e2dc52906721248a73e41124
Tien Dang <tien140801@gmail.com>
2022-11-07 16:31:44 +0700

    Update login and logout
    Update login and logout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Suspense } from 'react'
-import AppRouter from './AppRouter'
+import React, { Suspense } from 'react';
+
+import AppRouter from './AppRouter';
 
 function App() {
-  return (
-    <>
-      <Suspense fallback={<></>}>
-        <AppRouter></AppRouter>
-      </Suspense>
-    </>
-  )
+    return (
+        <>
+            <Suspense fallback={<></>}>
+                <AppRouter></AppRouter>
+            </Suspense>
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,74 +1,78 @@
-import React, { lazy } from 'react'
-import { Route, Routes } from 'react-router-dom'
-const MainLayout = lazy(() => import('./layout/MainLayout'))
-const TeacherDashBoard = lazy(() =>
-  import('./pages/TeacherPages/TeacherDashBoard'),
-)
-const TeacherClasses = lazy(() => import('./pages/TeacherPages/TeacherClasses'))
-const TeacherClass = lazy(() => import('./pages/TeacherPages/TeacherClass'))
-const TeacherCreateQuestion = lazy(() =>
-  import('./pages/TeacherPages/TeacherCreateQuestion'),
-)
-const Topics = lazy(() => import('./pages/TopicPages/Topics'))
-const Skills = lazy(() => import('./pages/SkillPages/Skills'))
-const TeacherAssignment = lazy(() =>
-  import('./pages/TeacherPages/TeacherAssignment'),
-)
-const TeacherManageStudents = lazy(() =>
-  import('./pages/TeacherPages/TeacherManageStudents'),
-)
-const TeacherManageTopic = lazy(() =>
-  import('./pages/TeacherPages/TeacherManageTopic'),
-)
+import React, { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+
+const MainLayout = lazy(() => import('./layout/MainLayout'));
+const TeacherDashBoard = lazy(() => import('./pages/TeacherPages/TeacherDashBoard'));
+const TeacherClasses = lazy(() => import('./pages/TeacherPages/TeacherClasses'));
+const TeacherClass = lazy(() => import('./pages/TeacherPages/TeacherClass'));
+const TeacherCreateQuestion = lazy(() => import('./pages/TeacherPages/TeacherCreateQuestion'));
+const Topics = lazy(() => import('./pages/TopicPages/Topics'));
+const Skills = lazy(() => import('./pages/SkillPages/Skills'));
+const TeacherAssignment = lazy(() => import('./pages/TeacherPages/TeacherAssignment'));
+const TeacherManageStudents = lazy(() => import('./pages/TeacherPages/TeacherManageStudents'));
+const TeacherManageTopic = lazy(() => import('./pages/TeacherPages/TeacherManageTopic'));
 // Students
-const StudentDashboard = lazy(() =>
-  import('./pages/StudentPages/StudentDashboard'),
-)
+const StudentDashboard = lazy(() => import('./pages/StudentPages/StudentDashboard'));
+const StudentClass = lazy(() => import('./pages/StudentPages/StudentClass'));
+const AnswerQuestion = lazy(() => import('./pages/AnswerQuestion'));
 
 //login
-const Login = lazy(() => import('./pages/Login'))
-
-const StudentClass = lazy(() => import('./pages/StudentPages/StudentClass'))\
-
-const AnswerQuestion = lazy(() => import('./pages/AnswerQuestion'))
+const Login = lazy(() => import('./pages/Login'));
+const NotFound = lazy(() => import('./pages/NotFound'));
 
 export default function AppRouter() {
+    const accessToken = localStorage.getItem('access_token');
+    const decodedToken = accessToken && jwt_decode(accessToken);
+    if (!decodedToken)
+        return (
+            <Routes>
+                <Route path='*' element={<Login />} />;
+            </Routes>
+        );
+    
+    return (
+        <Routes>
+            <Route path='/' element={<MainLayout role={decodedToken?.roleId} />}>
+                {decodedToken?.roleId === 2 ? (
+                    <>
+                        <Route path='/' element={<TeacherDashBoard />} />
+                        <Route path='/dashboard' element={<TeacherDashBoard />} />
+                        <Route path='/class' element={<TeacherClasses />} />
+                        <Route path='/class/:classId' element={<TeacherClass />} />
+                        <Route path='/class/:classId/topic' element={<Topics />} />
+                        <Route path='/class/:classId/topic/:topicId' element={<Skills />} />
+                        <Route path='/create-question' element={<TeacherCreateQuestion />} />
+                        <Route path='/manage-student' element={<TeacherManageStudents />} />
+                        <Route
+                            path='/skill/:skillId/assignment/:assignmentId'
+                            element={<TeacherAssignment />}
+                        />
+                        <Route
+                            path='/class/:classId/manage-topic'
+                            element={<TeacherManageTopic />}
+                        />
+                    </>
+                ) : decodedToken?.roleId === 3 ? (
+                    <>
+                        <Route path='/' element={<StudentDashboard />} />
+                        <Route path='/dashboard' element={<StudentDashboard />} />
+                        <Route
+                            path='/assignment/:assignmentId/question'
+                            element={<AnswerQuestion />}
+                        />
+                        <Route path='/class' element={<StudentClass />} />
+                        {/* login */}
+                    </>
+                ) : (
+                    <>
+                        <Route path='*' element={<Login />} />
+                    </>
+                )}
+            </Route>
 
-  return (
-    <Routes>
-      <Route path="/" element={<MainLayout></MainLayout>}>
-        <Route path="/" element={<TeacherDashBoard />}></Route>
-        <Route path="/teacher/dashboard" element={<TeacherDashBoard />}></Route>
-        <Route path="/teacher/class" element={<TeacherClasses />}></Route>
-        <Route path="/teacher/class/:classId" element={<TeacherClass />} />
-        <Route path="/teacher/class/:classId/topic" element={<Topics />} />
-        <Route
-          path="/teacher/class/:classId/topic/:topicId"
-          element={<Skills />}
-        />
-        <Route
-          path="/teacher/create-question"
-          element={<TeacherCreateQuestion />}
-        />
-        <Route
-          path="/teacher/manage-student"
-          element={<TeacherManageStudents />}
-        />
-        <Route
-          path="/teacher/skill/:skillId/assignment/:assignmentId"
-          element={<TeacherAssignment />}
-        />
-        <Route
-          path="/teacher/class/:classId/manage-topic"
-          element={<TeacherManageTopic />}
-        />
-        {/* student routes */}
-        <Route path="/student/dashboard" element={<StudentDashboard />} />
-        <Route path="/question" element={<AnswerQuestion />} />
-        <Route path="/student/class" element={<StudentClass />} />
-        {/* login */}
-      </Route>
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  )
+            <Route path='/login' element={<Login />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>
+    );
 }
