c718998cb1c38d70751fda514900b3fcdd79687d
Nhat Nguyen <nhatnm@primascx.net>
2022-11-26 14:39:42 +0700

    deploy edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   darkMode: 'class',
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{html,svelte,js,jsx,ts,tsx}'],
   theme: {
     extend: {
       backgroundImage: {
