1e640609514d360c5d13ab2478dab7aac92588b4
Tien Dang <tien140801@gmail.com>
2022-12-02 20:03:47 +0700

    Check student view button view result or do assignment

--- src/components/Student/SkillInTopics.js
+++ src/components/Student/SkillInTopics.js
@@ -1,115 +1,79 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { API_URL } from '../../constant'
-import createAxiosJWT from '../../createAxiosJWT'
-import Button from '../Button'
-import TokenExpire from '../Modals/TokenExpire'
+import React, { useCallback, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { API_URL } from '../../constant';
+import createAxiosJWT from '../../createAxiosJWT';
+import Button from '../Button';
+import TokenExpire from '../Modals/TokenExpire';
 
-const axiosJWT = createAxiosJWT()
+const axiosJWT = createAxiosJWT();
 
-const SkillInTopics = ({ isTeacher, val, getTopicOfClass }) => {
-  const navigate = useNavigate()
+const SkillInTopics = ({ val }) => {
+    const navigate = useNavigate();
+    const [assignmentsOfSkill, setAssignmentsOfSkill] = useState([]);
+    const [isExpired, setIsExpired] = useState(false);
 
-  const [openMoreOption, setOpenMoreOption] = useState(false)
-  const [assignmentsOfSkill, setAssignmentsOfSkill] = useState([])
-  const [isExpired, setIsExpired] = useState(false)
+    const handleGetAssignmentsOfSkill = useCallback(async () => {
+        try {
+            const res = await axiosJWT.get(
+                API_URL + `skill-assignment/student/skill/${val?.skill?.id}`
+            );
+            setAssignmentsOfSkill(res.data);
+        } catch (error) {
+            console.log(error);
+            if (error.response.status === 401) setIsExpired(true);
+        }
+    }, [val?.skill?.id]);
 
-  const handleGetAssignmentsOfSkill = useCallback(async () => {
-    try {
-      const res = await axiosJWT.get(
-        API_URL + `skill-assignment/skill/${val?.id}`,
-      )
-      setAssignmentsOfSkill(res.data)
-    } catch (error) {
-      console.log(error)
-      if (error.response.status === 401) setIsExpired(true)
-    }
-  }, [val])
+    const handleDoAssignment = async (id) => {
+        navigate(`/assignment/${id}/question/`);
+    };
 
-  const handleDoAssignment = async (id) => {
-    navigate(`/assignment/${id}/question/`)
-  }
+    useEffect(() => {
+        handleGetAssignmentsOfSkill();
+    }, [handleGetAssignmentsOfSkill]);
 
-  useEffect(() => {
-    handleGetAssignmentsOfSkill()
-  }, [handleGetAssignmentsOfSkill])
-
-  const handleDeleteSkill = async (id) => {
-    try {
-      await axiosJWT.delete(API_URL + `skill/${id}`)
-      getTopicOfClass()
-    } catch (error) {
-      console.log(error)
-      if (error.response.status === 401) setIsExpired(true)
-    }
-  }
-
-  return (
-    <div className="flex flex-col gap-3 px-2 py-3 ">
-      <div className="flex border-b p-y-2 pb-2 flex-row justify-between items-center">
-        <span className="text-xl">{val.skillName}</span>
-        <div className="flex flex-row gap-5 items-center">
-          <span className="text-primary text-sm">{val.standardName}</span>
-          {isTeacher && (
-            <div className="flex flex-col">
-              <div
-                className={`rounded-full relative h-[24px] w-[24px] cursor-pointer  select-none flex items-center justify-center bg-${
-                  openMoreOption ? `gray-100` : `white`
-                } hover:bg-gray-100`}
-                onClick={() => setOpenMoreOption(!openMoreOption)}
-              >
-                <i className="fas fa-ellipsis-h font-xs"></i>
-                {openMoreOption && (
-                  <div className="absolute z-1 w-[125px] translate-y-14 -translate-x-12 border-t-2 text-sm border-primary bg-[#ffffff] flex flex-col divide-y shadow-lg rounded-b">
-                    <div
-                      className="cursor-pointer px-2 py-1 hover:bg-[#ffffff] transition-all"
-                      onClick={() => {
-                        handleDeleteSkill(val.id)
-                      }}
-                    >
-                      <span>Remove</span>
-                    </div>
-                    <div className="cursor-pointer px-2 py-1 hover:bg-[#ffffff] transition-all">
-                      <span>Create Assignment</span>
-                    </div>
-                  </div>
-                )}
-              </div>
+    return (
+        <div className='flex flex-col gap-3 px-2 py-3 '>
+            <div className='flex border-b p-y-2 pb-2 flex-row justify-between items-center'>
+                <span className='text-xl'>{val?.skill.skillName}</span>
+                <div className='flex flex-row gap-5 items-center'>
+                    <span className='text-primary text-sm'>{val?.skill.standardName}</span>
+                </div>
             </div>
-          )}
+            <div className='flex flex-col gap-4'>
+                {assignmentsOfSkill?.map((assignmentOfSkill, i) => {
+                    return (
+                        <div key={i} className='flex flex-row items-center justify-between'>
+                            <span
+                                className='cursor-pointer'
+                                onClick={() => {
+                                    navigate(`/assignment-summary/${assignmentOfSkill.id}/`);
+                                }}
+                            >
+                                {assignmentOfSkill?.assignment?.assignmentName}
+                            </span>
+                            {!assignmentOfSkill?.numberQuestionOfAssignment ? undefined : !assignmentOfSkill
+                                  ?.assignment?.studentAssignment?.dateComplete ? (
+                                <Button
+                                    className='text-xs'
+                                    onClick={() =>
+                                        handleDoAssignment(assignmentOfSkill.assignment.id)
+                                    }
+                                >
+                                    Do assignment
+                                </Button>
+                            ) : (
+                                <Button className='text-xs' onClick={() => {}}>
+                                    View
+                                </Button>
+                            )}
+                        </div>
+                    );
+                })}
+            </div>
+            <TokenExpire isOpen={isExpired} />
         </div>
-      </div>
-      <div className="flex flex-col gap-4">
-        {assignmentsOfSkill?.map((val, i) => {
-          return (
-            <div key={i} className="flex flex-row items-center justify-between">
-              <span
-                className="cursor-pointer"
-                onClick={() => {
-                  navigate(`/assignment-summary/${val.id}/`)
-                }}
-              >
-                {val?.assignment?.assignmentName}
-              </span>
-              {false ? (
-                <Button
-                  className="text-xs"
-                  onClick={() => handleDoAssignment(val.assignment.id)}
-                >
-                  Do assignment
-                </Button>
-              ) : (
-                <Button className="text-xs" onClick={() => {}}>
-                  View
-                </Button>
-              )}
-            </div>
-          )
-        })}
-      </div>
-      <TokenExpire isOpen={isExpired} />
-    </div>
-  )
-}
+    );
+};
 
-export default SkillInTopics
+export default SkillInTopics;
--- src/pages/StudentPages/StudentClass.js
+++ src/pages/StudentPages/StudentClass.js
@@ -1,218 +1,210 @@
-import React, { useEffect, useState, useContext, useMemo } from 'react'
-import { useCallback } from 'react'
-import 'react-circular-progressbar/dist/styles.css'
-import { useNavigate, useParams } from 'react-router-dom'
-import jwtDecode from 'jwt-decode'
+import React, { useEffect, useState, useContext, useMemo } from 'react';
+import { useCallback } from 'react';
+import 'react-circular-progressbar/dist/styles.css';
+import { useNavigate, useParams } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 
 // Import Swiper styles
-import 'swiper/css'
-import 'swiper/css/navigation'
-import Button from '../../components/Button'
-import TokenExpire from '../../components/Modals/TokenExpire'
-import TopicCard from '../../components/Student/TopicCard'
-import { API_URL } from '../../constant'
-import createAxiosJWT from '../../createAxiosJWT'
-import { SocketContext } from '../../App'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import Button from '../../components/Button';
+import TokenExpire from '../../components/Modals/TokenExpire';
+import TopicCard from '../../components/Student/TopicCard';
+import { API_URL } from '../../constant';
+import createAxiosJWT from '../../createAxiosJWT';
+import { SocketContext } from '../../App';
 
-import nocontent from '../../assets/image/nocontent.png'
-import SkillInTopics from '../../components/Student/SkillInTopics'
+import nocontent from '../../assets/image/nocontent.png';
+import SkillInTopics from '../../components/Student/SkillInTopics';
 
-const axiosJWT = createAxiosJWT()
+const axiosJWT = createAxiosJWT();
 
 const topicImage =
-  'https://thumbs.dreamstime.com/b/letter-block-word-topic-wood-background-business-concept-170764857.jpg'
+    'https://thumbs.dreamstime.com/b/letter-block-word-topic-wood-background-business-concept-170764857.jpg';
 const StudentClass = () => {
-  const socket = useContext(SocketContext)
-  const accessToken = localStorage.getItem('access_token')
-  const decodedToken = useMemo(() => {
-    return accessToken && jwtDecode(accessToken)
-  }, [accessToken])
+    const socket = useContext(SocketContext);
+    const accessToken = localStorage.getItem('access_token');
+    const decodedToken = useMemo(() => {
+        return accessToken && jwtDecode(accessToken);
+    }, [accessToken]);
 
-  const { classId } = useParams()
-  const [topicsOfClass, setTopicsOfClass] = useState([])
-  const [searchTerm, setSearchTerm] = useState('')
-  const [currentTopic, setCurrentTopic] = useState()
-  const [skills, setSkills] = useState([])
-  const [currentTopicId, setCurrentTopicId] = useState('')
-  const [topics, setTopics] = useState()
-  const [isExpired, setIsExpired] = useState(false)
+    const { classId } = useParams();
+    const [topicsOfClass, setTopicsOfClass] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [currentTopic, setCurrentTopic] = useState();
+    const [skills, setSkills] = useState([]);
+    const [currentTopicId, setCurrentTopicId] = useState('');
+    const [topics, setTopics] = useState();
+    const [isExpired, setIsExpired] = useState(false);
 
-  const getTopicIdUnlocked = (topics) => {
-    for (let i = 0; i < topics.length; i++)
-      if (topics[i].isUnlock === 1) return topics[i].topicId
-  }
-  const getTopicOfClass = useCallback(async () => {
-    try {
-      const topicsOfStudent = await axiosJWT.put(
-        API_URL + `student-topic/class/${classId}`,
-      )
-      const result = topicsOfStudent.data
-      if (!currentTopicId) setCurrentTopicId(getTopicIdUnlocked(result))
-      let valueTopics = []
-      for (let i = 0; i < result.length; ++i) {
-        valueTopics = [
-          ...valueTopics,
-          {
-            id: result[i].topicId,
-            topicName: result[i].topicName,
-            description: result[i].description,
-            topicImg: result[i].topicImg,
-            prerequisiteTopicName: result[i].prerequisiteTopicName,
-            classTopicId: result[i].id,
-          },
-        ]
-      }
-      setTopics(valueTopics)
-      setTopicsOfClass(result)
-    } catch (error) {
-      console.log(error)
-      if (error.response.status === 401) setIsExpired(true)
-    }
-  }, [classId, currentTopicId])
+    const getTopicIdUnlocked = (topics) => {
+        for (let i = 0; i < topics.length; i++)
+            if (topics[i].isUnlock === 1) return topics[i].topicId;
+    };
+    const getTopicOfClass = useCallback(async () => {
+        try {
+            const topicsOfStudent = await axiosJWT.put(API_URL + `student-topic/class/${classId}`);
+            const result = topicsOfStudent.data;
+            if (!currentTopicId) setCurrentTopicId(getTopicIdUnlocked(result));
+            let valueTopics = [];
+            for (let i = 0; i < result.length; ++i) {
+                valueTopics = [
+                    ...valueTopics,
+                    {
+                        id: result[i].topicId,
+                        topicName: result[i].topicName,
+                        description: result[i].description,
+                        topicImg: result[i].topicImg,
+                        prerequisiteTopicName: result[i].prerequisiteTopicName,
+                        classTopicId: result[i].id,
+                    },
+                ];
+            }
+            setTopics(valueTopics);
+            setTopicsOfClass(result);
+        } catch (error) {
+            console.log(error);
+            if (error.response.status === 401) setIsExpired(true);
+        }
+    }, [classId, currentTopicId]);
 
-  const getSkillsOfTopic = useCallback(async () => {
-    try {
-      if (currentTopicId) {
-        const res = await axiosJWT.get(
-          API_URL + `skill/topic/${currentTopicId}`,
-        )
-        setSkills(res.data)
-        setCurrentTopic(
-          topics.find((topic) => topic.id === currentTopicId) || {},
-        )
-      }
-    } catch (error) {
-      console.log(error)
-      if (error.response.status === 401) setIsExpired(true)
-    }
-  }, [currentTopicId, topics])
+    const getSkillsOfTopic = useCallback(async () => {
+        try {
+            if (currentTopicId) {
+                const res = await axiosJWT.get(
+                    API_URL + `student-skill/student/topic/${currentTopicId}`
+                );
+                setSkills(res.data);
+                setCurrentTopic(topics.find((topic) => topic.id === currentTopicId) || {});
+            }
+        } catch (error) {
+            console.log(error);
+            if (error.response.status === 401) setIsExpired(true);
+        }
+    }, [currentTopicId, topics]);
 
-  useEffect(() => {
-    getSkillsOfTopic()
-  }, [getSkillsOfTopic])
+    useEffect(() => {
+        getSkillsOfTopic();
+    }, [getSkillsOfTopic]);
 
-  useEffect(() => {
-    getTopicOfClass()
-  }, [getTopicOfClass])
+    useEffect(() => {
+        getTopicOfClass();
+    }, [getTopicOfClass]);
 
-  useEffect(() => {
-    socket?.on('get-handle-request-notification', (data) => {
-      if (
-        data?.senderId !== decodedToken?.accountId &&
-        data?.tableHandle === 'Student_Topic'
-      ) {
-        getTopicOfClass()
-      }
-    })
-    return () => {
-      socket?.off('get-handle-request-notification')
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [socket, decodedToken])
+    useEffect(() => {
+        socket?.on('get-handle-request-notification', (data) => {
+            if (
+                data?.senderId !== decodedToken?.accountId &&
+                data?.tableHandle === 'Student_Topic'
+            ) {
+                getTopicOfClass();
+            }
+        });
+        return () => {
+            socket?.off('get-handle-request-notification');
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [socket, decodedToken]);
 
-  return (
-    <div className="flex flex-row h-screen">
-      {/* left */}
-      <div className="w-[40%] flex flex-col px-5 py-5 gap-6">
-        <div className="flex flex-col gap-3 px-4">
-          <span className="text-2xl font-medium truncate">ClassName</span>
-          {/* search */}
-          <div className="flex flex-row justify-between items-center w-full">
-            <input
-              onChange={(e) => setSearchTerm(e.target.value)}
-              className="outline-none rounded-lg text-base px-4 py-2 w-[65%] drop-shadow-md focus:drop-shadow-lg transition-all"
-              placeholder="Search topics"
-            />
-          </div>
+    return (
+        <div className='flex flex-row h-screen'>
+            {/* left */}
+            <div className='w-[40%] flex flex-col px-5 py-5 gap-6'>
+                <div className='flex flex-col gap-3 px-4'>
+                    <span className='text-2xl font-medium truncate'>ClassName</span>
+                    {/* search */}
+                    <div className='flex flex-row justify-between items-center w-full'>
+                        <input
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                            className='outline-none rounded-lg text-base px-4 py-2 w-[65%] drop-shadow-md focus:drop-shadow-lg transition-all'
+                            placeholder='Search topics'
+                        />
+                    </div>
+                </div>
+                {/* filter */}
+                {/* courses */}
+                <div className='flex flex-col gap-7 px-4 pb-4 overflow-auto scroll-smooth'>
+                    {topicsOfClass
+                        .filter((val) => {
+                            if (
+                                searchTerm === '' ||
+                                val.topicName.toLowerCase().includes(searchTerm.toLowerCase())
+                            )
+                                return val;
+                            return '';
+                        })
+                        .map((item, i) => {
+                            return (
+                                <TopicCard
+                                    topicInfo={item}
+                                    key={i}
+                                    setCurrentTopicId={setCurrentTopicId}
+                                />
+                            );
+                        })}
+                </div>
+            </div>
+            {/* right */}
+            <div className='flex flex-col gap-5 w-[60%] bg-white py-5 px-10 overflow-y-auto'>
+                {/* Topic tile */}
+                {currentTopic ? (
+                    <>
+                        <div className='flex flex-row gap-3 justify-center items-center'>
+                            <span className='text-2xl text-primary'>{currentTopic?.topicName}</span>
+                        </div>
+                        {/* image */}
+                        <div
+                            className={`relative w-full min-h-[400px] overflow-hidden flex items-center justify-center bg-center select-none transition-all`}
+                        >
+                            <img
+                                src={currentTopic?.topicImg || topicImage}
+                                className='w-[500px] rounded-lg h-fit object-contain'
+                                alt=''
+                            />
+                        </div>
+                        {/* topic des */}
+                        <div className='flex flex-col gap-3'>
+                            <div className='flex flex-row justify-between items-center'>
+                                <span className='text-2xl font-medium text-gray-700'>
+                                    Topic descriptions
+                                </span>
+                            </div>
+                            {currentTopic?.description && (
+                                <span className='px-2 text-justify'>
+                                    {currentTopic?.description}
+                                </span>
+                            )}
+                        </div>
+                        {/* skills */}
+                        <div className='flex flex-col gap-3'>
+                            <div className='flex flex-row justify-between items-center'>
+                                <span className='text-2xl font-medium text-gray-700'>Skills</span>
+                            </div>
+                            <div className='flex flex-col gap-1'>
+                                {skills.map((val, i) => {
+                                    return (
+                                        <SkillInTopics
+                                            val={val}
+                                            key={val.id + i}
+                                            getTopicOfClass={getTopicOfClass}
+                                        />
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    </>
+                ) : (
+                    <div className='w-full h-full flex flex-col gap-5 items-center justify-center'>
+                        <img src={nocontent} alt='' className='h-[300px]' />
+                        <span className='text-2xl font-[500] text-primary'>
+                            No content, choose 1 topic to view
+                        </span>
+                    </div>
+                )}
+            </div>
+            <TokenExpire isOpen={isExpired} />
         </div>
-        {/* filter */}
-        {/* courses */}
-        <div className="flex flex-col gap-7 px-4 pb-4 overflow-auto scroll-smooth">
-          {topicsOfClass
-            .filter((val) => {
-              if (
-                searchTerm === '' ||
-                val.topicName.toLowerCase().includes(searchTerm.toLowerCase())
-              )
-                return val
-              return ''
-            })
-            .map((item, i) => {
-              return (
-                <TopicCard
-                  topicInfo={item}
-                  key={i}
-                  setCurrentTopicId={setCurrentTopicId}
-                />
-              )
-            })}
-        </div>
-      </div>
-      {/* right */}
-      <div className="flex flex-col gap-5 w-[60%] bg-white py-5 px-10 overflow-y-auto">
-        {/* Topic tile */}
-        {currentTopic ? (
-          <>
-            <div className="flex flex-row gap-3 justify-center items-center">
-              <span className="text-2xl text-primary">
-                {currentTopic?.topicName}
-              </span>
-            </div>
-            {/* image */}
-            <div
-              className={`relative w-full min-h-[400px] overflow-hidden flex items-center justify-center bg-center select-none transition-all`}
-            >
-              <img
-                src={currentTopic?.topicImg || topicImage}
-                className="w-[500px] rounded-lg h-fit object-contain"
-                alt=""
-              />
-            </div>
-            {/* topic des */}
-            <div className="flex flex-col gap-3">
-              <div className="flex flex-row justify-between items-center">
-                <span className="text-2xl font-medium text-gray-700">
-                  Topic descriptions
-                </span>
-              </div>
-              {currentTopic?.description && (
-                <span className="px-2 text-justify">
-                  {currentTopic?.description}
-                </span>
-              )}
-            </div>
-            {/* skills */}
-            <div className="flex flex-col gap-3">
-              <div className="flex flex-row justify-between items-center">
-                <span className="text-2xl font-medium text-gray-700">
-                  Skills
-                </span>
-              </div>
-              <div className="flex flex-col gap-1">
-                {skills.map((val, i) => {
-                  return (
-                    <SkillInTopics
-                      val={val}
-                      key={val.id + i}
-                      getTopicOfClass={getTopicOfClass}
-                    />
-                  )
-                })}
-              </div>
-            </div>
-          </>
-        ) : (
-          <div className="w-full h-full flex flex-col gap-5 items-center justify-center">
-            <img src={nocontent} alt="" className="h-[300px]" />
-            <span className="text-2xl font-[500] text-primary">
-              No content, choose 1 topic to view
-            </span>
-          </div>
-        )}
-      </div>
-      <TokenExpire isOpen={isExpired} />
-    </div>
-  )
-}
+    );
+};
 
-export default StudentClass
+export default StudentClass;
