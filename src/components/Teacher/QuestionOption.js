c4fde61d7014b9f7020611e745d60bca69d8f2b4
Nhat Nguyen <nhatnm@primascx.net>
2022-12-05 11:45:56 +0700

    dap lai cai question option

--- src/components/Teacher/QuestionOption.js
+++ src/components/Teacher/QuestionOption.js
@@ -1,107 +1,58 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 import Select from 'react-select'
-import AsyncSelect from 'react-select/async'
 
-import { API_URL } from '../../constant'
-import createAxiosJWT from '../../createAxiosJWT'
-
-const axiosJWT = createAxiosJWT()
 const QuestionOption = ({
+  listSkill,
   selectedSkills,
   setSelectedSkills,
-  setSelectedLevel,
 
-  selectedLevel,
+  listGrade,
   setSelectedGrade,
   selectedGrade,
 
-  setSelectedTopic,
+  setSelectedLevel,
+  selectedLevel,
+
+  listTopic,
   selectedTopic,
+  setSelectedTopic,
+
+  levelOption,
+
   handleScore,
   score,
 }) => {
-  const levelOption = [
-    { value: 'Easy', label: 'Easy' },
-    { value: 'Medium', label: 'Medium' },
-    { value: 'Hard', label: 'Hard' },
-  ]
-
-  const [listGrade, setListGrade] = useState([])
-  const [listTopic, setListTopic] = useState([])
-  const [listSkill, setListSkill] = useState([])
-
-  const selectLevel = useRef()
-  const selectGrade = useRef()
-  const selectTopic = useRef()
-  const selectSkill = useRef()
-
-  const prevSkill = useRef(selectedSkills[0])
-
-  const convertResToOption = (value, label) => {
-    return {
-      value: value,
-      label: label,
+  useEffect(() => {
+    if (listGrade.length > 0) {
+      setSelectedGrade(listGrade[0])
     }
-  }
-
-  useEffect(() => {
-    axiosJWT.get(API_URL + `grade/teacher`).then((res) => {
-      const grades = res.data
-      const option = []
-      for (let i = 0; i < grades.length; i++)
-        option.push(convertResToOption(grades[i].id, grades[i].gradeName))
-      setListGrade(option)
-    })
-  }, [])
-
-  useEffect(() => {
-    axiosJWT
-      .get(API_URL + `topic/teacher/grade/${selectedGrade}`)
-      .then((res) => {
-        const topics = res.data
-        const option = []
-        for (let i = 0; i < topics.length; i++)
-          option.push(convertResToOption(topics[i].id, topics[i].topicName))
-        setListTopic(option)
-        setSelectedTopic(option[0])
-      })
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedGrade])
+  }, [listGrade])
 
   useEffect(() => {
-    axiosJWT.get(API_URL + `skill/topic/${selectedTopic}`).then((res) => {
-      const skills = res.data
-      const option = []
-      for (let i = 0; i < skills.length; i++)
-        option.push(convertResToOption(skills[i].id, skills[i].skillName))
-      setListSkill(option)
-    })
+    if (listTopic.length > 0) {
+      setSelectedTopic(listTopic[0])
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedTopic])
+  }, [listTopic])
 
-  //   useEffect(() => {
-  //     if (selectedGrade)
-  //       axiosJWT
-  //         .get(API_URL + `topic/teacher/grade/${selectedGrade}`)
-  //         .then((res) => {
-  //           const topics = res.data
-  //           const option = []
-  //           for (let i = 0; i < topics.length; i++)
-  //             option.push(convertResToOption(topics[i].id, topics[i].topicName))
-  //           setListTopic(option)
-  //         })
-  //   }, [selectedGrade])
+  useEffect(() => {
+    if (listSkill.length > 0) {
+      setSelectedSkills([listSkill[0]])
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [listSkill])
 
-  //   useEffect(() => {
-  //     if (selectedTopic)
-  //       axiosJWT.get(API_URL + `skill/topic/${selectedTopic}`).then((res) => {
-  //         const skills = res.data
-  //         const option = []
-  //         for (let i = 0; i < skills.length; i++)
-  //           option.push(convertResToOption(skills[i].id, skills[i].skillName))
-  //         setListSkill(option)
-  //       })
-  //   }, [selectedTopic])
+  // useEffect(() => {
+  //   if (selectedTopic)
+  //     axiosJWT.get(API_URL + `skill/topic/${selectedTopic}`).then((res) => {
+  //       const skills = res.data
+  //       const option = []
+  //       for (let i = 0; i < skills.length; i++)
+  //         option.push(convertResToOption(skills[i].id, skills[i].skillName))
+  //       setListSkill(option)
+  //     })
+  // }, [selectedTopic])
 
   //   useEffect(() => {
   //     if (selectedLevel)
@@ -111,15 +62,15 @@ const QuestionOption = ({
   //     else selectLevel.current.setValue('')
   //   }, [selectedLevel])
 
-  //   useEffect(() => {
-  //     if (listGrade.length > 0) {
-  //       const grade = listGrade.find((grade) => grade.value === selectedGrade)
-  //       if (grade)
-  //         selectGrade.current.setValue(
-  //           convertResToOption(selectedGrade, grade.label),
-  //         )
-  //     }
-  //   }, [selectedGrade, listGrade])
+  // useEffect(() => {
+  //   if (listGrade.length > 0) {
+  //     const grade = listGrade.find((grade) => grade.value === selectedGrade)
+  //     if (grade)
+  //       selectGrade.current.setValue(
+  //         convertResToOption(selectedGrade, grade.label),
+  //       )
+  //   }
+  // }, [selectedGrade, listGrade])
 
   //   useEffect(() => {
   //     if (listTopic.length > 0) {
@@ -152,23 +103,22 @@ const QuestionOption = ({
   //   }, [selectedSkills, listSkill])
 
   return (
-    <div>
+    <>
       <div className="flex flex-col justify-between items-center gap-3">
         <div className="flex flex-start justify-between gap-2 w-full">
           <Select
-            defaultValue={listGrade[0]}
-            onChange={(e) => setSelectedGrade(e.value)}
+            value={selectedGrade}
+            onChange={setSelectedGrade}
             options={listGrade}
             placeholder="Grade"
-            className="w-[50%] "
+            className="w-[50%]"
           />
 
           <Select
-            defaultValue={levelOption[0]}
+            value={selectedLevel}
             options={levelOption}
             placeholder="Level"
-            onChange={(e) => setSelectedLevel(e.value)}
-            setValue={selectedLevel}
+            onChange={setSelectedLevel}
             className="w-[50%] max-w-[288px]"
           />
 
@@ -183,23 +133,27 @@ const QuestionOption = ({
           </div>
         </div>
         <div className="flex w-full gap-2">
-          <Select
-            onChange={(e) => setSelectedTopic(e.value)}
-            defaultValue={listTopic[0]}
-            options={listTopic}
-            placeholder="Topic"
-            className="min-w-[288px] max-w-[288px]"
-          />
-          <Select
-            defaultValue={listSkill[0]}
-            onChange={(e) => setSelectedSkills([e.value])}
-            options={listSkill}
-            placeholder="Skill"
-            className="grow"
-          />
+          {listTopic?.length > 0 && (
+            <Select
+              value={selectedTopic}
+              onChange={setSelectedTopic}
+              options={listTopic}
+              placeholder="Topic"
+              className="min-w-[288px] max-w-[288px]"
+            />
+          )}
+          {listSkill?.length > 0 && (
+            <Select
+              value={selectedSkills}
+              onChange={setSelectedSkills}
+              options={listSkill}
+              placeholder="Skill"
+              className="grow"
+            />
+          )}
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
