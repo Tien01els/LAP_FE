c8858ebbd0fbb6c0f62748de6279933b72472e61
Tien Dang <tien140801@gmail.com>
2022-12-06 15:53:54 +0700

    Teacher view assignment summary

--- src/components/Teacher/TeacherSkillInTopics.js
+++ src/components/Teacher/TeacherSkillInTopics.js
@@ -1,138 +1,136 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { API_URL } from '../../constant'
-import createAxiosJWT from '../../createAxiosJWT'
-import ModalAssign from '../Modals/ModalAssign'
-import TokenExpire from '../Modals/TokenExpire'
+import React, { useCallback, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { API_URL } from '../../constant';
+import createAxiosJWT from '../../createAxiosJWT';
+import ModalAssign from '../Modals/ModalAssign';
+import TokenExpire from '../Modals/TokenExpire';
 
-const axiosJWT = createAxiosJWT()
+const axiosJWT = createAxiosJWT();
 
 const TeacherSkillInTopics = ({ isTeacher, val, getTopicOfClass }) => {
-  const navigate = useNavigate()
+    const navigate = useNavigate();
 
-  const [openMoreOption, setOpenMoreOption] = useState(false)
-  const [modalAssignIsOpen, setAssignIsOpen] = useState(false)
-  const [assignmentsOfSkill, setAssignmentsOfSkill] = useState([])
-  const [isExpired, setIsExpired] = useState(false)
+    const [openMoreOption, setOpenMoreOption] = useState(false);
+    const [modalAssignIsOpen, setAssignIsOpen] = useState(false);
+    const [assignmentsOfSkill, setAssignmentsOfSkill] = useState([]);
+    const [isExpired, setIsExpired] = useState(false);
 
-  const handleOpenModalAssign = () => {
-    setAssignIsOpen(true)
-  }
+    const handleOpenModalAssign = () => {
+        setAssignIsOpen(true);
+    };
 
-  const handleGetAssignmentsOfSkill = useCallback(async () => {
-    try {
-      const res = await axiosJWT.get(
-        API_URL + `skill-assignment/skill/${val?.id}`,
-      )
-      setAssignmentsOfSkill(res.data)
-    } catch (error) {
-      console.log(error)
-      if (error.response.status === 401) setIsExpired(true)
-    }
-  }, [val])
+    const handleGetAssignmentsOfSkill = useCallback(async () => {
+        try {
+            const res = await axiosJWT.get(API_URL + `skill-assignment/skill/${val?.id}`);
+            setAssignmentsOfSkill(res.data);
+        } catch (error) {
+            console.log(error);
+            if (error.response.status === 401) setIsExpired(true);
+        }
+    }, [val]);
 
-  const handleEditAssignment = async (id) => {
-    navigate(`/skill/${val.id}/assignment/${id}/`)
-  }
+    const handleEditAssignment = async (id) => {
+        navigate(`/skill/${val.id}/assignment/${id}/`);
+    };
 
-  const handleRemoveAssignment = async (id) => {
-    try {
-      await axiosJWT.delete(API_URL + `skill-assignment/${id}`)
-      getTopicOfClass()
-    } catch (error) {
-      console.log(error)
-      if (error.response.status === 401) setIsExpired(true)
-    }
-  }
+    const handleRemoveAssignment = async (id) => {
+        try {
+            await axiosJWT.delete(API_URL + `skill-assignment/${id}`);
+            getTopicOfClass();
+        } catch (error) {
+            console.log(error);
+            if (error.response.status === 401) setIsExpired(true);
+        }
+    };
 
-  useEffect(() => {
-    handleGetAssignmentsOfSkill()
-  }, [handleGetAssignmentsOfSkill])
+    useEffect(() => {
+        handleGetAssignmentsOfSkill();
+    }, [handleGetAssignmentsOfSkill]);
 
-  const handleDeleteSkill = async (id) => {
-    try {
-      await axiosJWT.delete(API_URL + `skill/${id}`)
-      getTopicOfClass()
-    } catch (error) {
-      console.log(error)
-      if (error.response.status === 401) setIsExpired(true)
-    }
-  }
+    const handleDeleteSkill = async (id) => {
+        try {
+            await axiosJWT.delete(API_URL + `skill/${id}`);
+            getTopicOfClass();
+        } catch (error) {
+            console.log(error);
+            if (error.response.status === 401) setIsExpired(true);
+        }
+    };
 
-  return (
-    <div className="flex flex-col gap-3 px-2 py-3 ">
-      <div className="flex border-b p-y-2 pb-2 flex-row justify-between items-center">
-        <span className="text-xl w-[80%] truncate">{val.skillName}</span>
-        <div className="flex flex-row gap-5 items-center">
-          <span className="text-primary text-sm">{val.standardName}</span>
-          {isTeacher && (
-            <div className="flex flex-col">
-              <div
-                className={`rounded-full relative h-[24px] w-[24px] cursor-pointer  select-none flex items-center justify-center bg-${
-                  openMoreOption ? `gray-100` : `white`
-                } hover:bg-gray-100`}
-                onClick={() => setOpenMoreOption(!openMoreOption)}
-              >
-                <i className="fas fa-ellipsis-h font-xs"></i>
-                {openMoreOption && (
-                  <div className="absolute z-1 w-[125px] translate-y-14 -translate-x-12 border-t-2 text-sm border-primary bg-[#ffffff] flex flex-col divide-y shadow-lg rounded-b">
-                    <div
-                      className="cursor-pointer px-2 py-1 hover:bg-[#ffffff] transition-all"
-                      onClick={() => {
-                        handleDeleteSkill(val.id)
-                      }}
-                    >
-                      <span>Remove</span>
-                    </div>
-                    <div
-                      className="cursor-pointer px-2 py-1 hover:bg-[#ffffff] transition-all"
-                      onClick={handleOpenModalAssign}
-                    >
-                      <span>Create Assignment</span>
-                    </div>
-                  </div>
-                )}
-              </div>
+    return (
+        <div className='flex flex-col gap-3 px-2 py-3 '>
+            <div className='flex border-b p-y-2 pb-2 flex-row justify-between items-center'>
+                <span className='text-xl w-[80%] truncate'>{val.skillName}</span>
+                <div className='flex flex-row gap-5 items-center'>
+                    <span className='text-primary text-sm'>{val.standardName}</span>
+                    {isTeacher && (
+                        <div className='flex flex-col'>
+                            <div
+                                className={`rounded-full relative h-[24px] w-[24px] cursor-pointer  select-none flex items-center justify-center bg-${
+                                    openMoreOption ? `gray-100` : `white`
+                                } hover:bg-gray-100`}
+                                onClick={() => setOpenMoreOption(!openMoreOption)}
+                            >
+                                <i className='fas fa-ellipsis-h font-xs'></i>
+                                {openMoreOption && (
+                                    <div className='absolute z-1 w-[125px] translate-y-14 -translate-x-12 border-t-2 text-sm border-primary bg-[#ffffff] flex flex-col divide-y shadow-lg rounded-b'>
+                                        <div
+                                            className='cursor-pointer px-2 py-1 hover:bg-[#ffffff] transition-all'
+                                            onClick={() => {
+                                                handleDeleteSkill(val.id);
+                                            }}
+                                        >
+                                            <span>Remove</span>
+                                        </div>
+                                        <div
+                                            className='cursor-pointer px-2 py-1 hover:bg-[#ffffff] transition-all'
+                                            onClick={handleOpenModalAssign}
+                                        >
+                                            <span>Create Assignment</span>
+                                        </div>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                    )}
+                </div>
             </div>
-          )}
+            <ModalAssign
+                modalAssignIsOpen={modalAssignIsOpen}
+                setAssignIsOpen={setAssignIsOpen}
+                assignId={val.id}
+                assignmentName={val.skillName}
+                typeAssignment={'Skill'}
+            />
+            <div className='flex flex-col gap-4'>
+                {assignmentsOfSkill?.map((val, i) => {
+                    return (
+                        <div key={i} className='flex flex-row items-center justify-between'>
+                            <span
+                                className='cursor-pointer w-[80%] truncate'
+                                onClick={() => {
+                                    navigate(`/assignment-summary/${val?.assignment.id}/`);
+                                }}
+                            >
+                                {val?.assignment?.assignmentName}
+                            </span>
+                            <div className='flex flex-row items-center gap-3'>
+                                <i
+                                    className='fa-solid fa-pen-to-square text-primary cursor-pointer active:scale-90'
+                                    onClick={() => handleEditAssignment(val.assignment?.id)}
+                                ></i>
+                                <i
+                                    className='fa-solid fa-trash-can text-red-500 cursor-pointer active:scale-90'
+                                    onClick={() => handleRemoveAssignment(val.id)}
+                                ></i>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+            <TokenExpire isOpen={isExpired} />
         </div>
-      </div>
-      <ModalAssign
-        modalAssignIsOpen={modalAssignIsOpen}
-        setAssignIsOpen={setAssignIsOpen}
-        assignId={val.id}
-        assignmentName={val.skillName}
-        typeAssignment={'Skill'}
-      />
-      <div className="flex flex-col gap-4">
-        {assignmentsOfSkill?.map((val, i) => {
-          return (
-            <div key={i} className="flex flex-row items-center justify-between">
-              <span
-                className="cursor-pointer w-[80%] truncate"
-                onClick={() => {
-                  navigate(`/assignment-summary/${val.id}/`)
-                }}
-              >
-                {val?.assignment?.assignmentName}
-              </span>
-              <div className="flex flex-row items-center gap-3">
-                <i
-                  className="fa-solid fa-pen-to-square text-primary cursor-pointer active:scale-90"
-                  onClick={() => handleEditAssignment(val.assignment?.id)}
-                ></i>
-                <i
-                  className="fa-solid fa-trash-can text-red-500 cursor-pointer active:scale-90"
-                  onClick={() => handleRemoveAssignment(val.id)}
-                ></i>
-              </div>
-            </div>
-          )
-        })}
-      </div>
-      <TokenExpire isOpen={isExpired} />
-    </div>
-  )
-}
+    );
+};
 
-export default TeacherSkillInTopics
+export default TeacherSkillInTopics;
--- src/pages/TopicPages/ModalCreateTopic.js
+++ src/pages/TopicPages/ModalCreateTopic.js
@@ -1,237 +1,228 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { useForm, Controller } from 'react-hook-form'
-import Modal from 'react-modal'
+import React, { useEffect, useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import Modal from 'react-modal';
 
-import { API_URL } from '../../constant'
-import Button from '../../components/Button'
-import createAxiosJWT from '../../createAxiosJWT'
-import { toast } from 'react-toastify'
-import Select from 'react-select'
+import { API_URL } from '../../constant';
+import Button from '../../components/Button';
+import createAxiosJWT from '../../createAxiosJWT';
+import { toast } from 'react-toastify';
+import Select from 'react-select';
 
-const axiosJWT = createAxiosJWT()
+const axiosJWT = createAxiosJWT();
 const ModalCreateTopic = ({
-  modalTopicIsOpen,
-  setTopicIsOpen,
-  handleCloseModalAddTopic,
-  getTopicOfClass,
-  classId,
+    modalTopicIsOpen,
+    setTopicIsOpen,
+    handleCloseModalAddTopic,
+    getTopicOfClass,
+    classId,
 }) => {
-  const {
-    register: registerCreate,
-    handleSubmit: handleSubmitCreate,
-    reset: resetCreate,
-    formState: formStateCreate,
-    watch,
-    control,
-  } = useForm()
+    const {
+        register: registerCreate,
+        handleSubmit: handleSubmitCreate,
+        reset: resetCreate,
+        formState: formStateCreate,
+        watch,
+        control,
+    } = useForm();
 
-  const [grades, setGrades] = useState([])
-  const [prerequisiteTopicGrades, setPrerequisiteTopicGrades] = useState([])
+    const [grades, setGrades] = useState([]);
+    const [prerequisiteTopicGrades, setPrerequisiteTopicGrades] = useState([]);
 
-  const getAllGrades = async () => {
-    const res = await axiosJWT.get(API_URL + `grade`)
-    setGrades(res.data)
-  }
+    const getAllGrades = async () => {
+        const res = await axiosJWT.get(API_URL + `grade`);
+        setGrades(res.data);
+    };
 
-  function handleCloseModalCreateTopic() {
-    setTopicIsOpen(false)
-  }
-
-  useEffect(() => {
-    getAllGrades()
-    // getPrerequisiteTopicOfGrade()
-  }, [])
-
-  useEffect(() => {
-    if (formStateCreate.isSubmitSuccessful) {
-      resetCreate({
-        topicName: '',
-        description: '',
-        gradeId: -1,
-        prerequisiteTopicId: -1,
-      })
-      handleCloseModalCreateTopic()
+    function handleCloseModalCreateTopic() {
+        setTopicIsOpen(false);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [formStateCreate, resetCreate])
 
-  const handleCreateTopic = async (data) => {
-    try {
-      const prerequisiteTopic = data.prerequisiteTopicId?.id
-        ? {
-            prerequisiteTopicId: data.prerequisiteTopicId.id,
-          }
-        : {}
-      const topic = {
-        topicName: data.topicName,
-        description: data.description,
-        gradeId: data.gradeId.id,
-        ...prerequisiteTopic,
-      }
-      const res = await axiosJWT.post(API_URL + `topic`, topic)
-      if (res.data) {
-        await axiosJWT.post(API_URL + `class-topic`, {
-          topicId: res.data.topic?.id,
-          isUnlock: !data.isLock,
-          classId,
-        })
-        getTopicOfClass()
-      }
-    } catch (err) {
-      console.log(err)
-      toast('Create topic failed')
-    }
-    handleCloseModalAddTopic()
-  }
+    useEffect(() => {
+        getAllGrades();
+        // getPrerequisiteTopicOfGrade()
+    }, []);
 
-  const customStyles = {
-    overlay: {
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      backgroundColor: 'rgba(165, 165, 165, 0.6)',
-    },
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-      border: 'none',
-      borderRadius: '8px',
-    },
-  }
+    useEffect(() => {
+        if (formStateCreate.isSubmitSuccessful) {
+            resetCreate({
+                topicName: '',
+                description: '',
+                gradeId: -1,
+                prerequisiteTopicId: -1,
+            });
+            handleCloseModalCreateTopic();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [formStateCreate, resetCreate]);
 
-  let watchGrade = watch('gradeId')
+    const handleCreateTopic = async (data) => {
+        try {
+            const prerequisiteTopic = data.prerequisiteTopicId?.id
+                ? {
+                      prerequisiteTopicId: data.prerequisiteTopicId.id,
+                  }
+                : {};
+            const topic = {
+                topicName: data.topicName,
+                description: data.description,
+                gradeId: data.gradeId.id,
+                ...prerequisiteTopic,
+            };
+            const res = await axiosJWT.post(API_URL + `topic`, topic);
+            if (res.data) {
+                await axiosJWT.post(API_URL + `class-topic`, {
+                    topicId: res.data.topic?.id,
+                    isUnlock: !data.isLock,
+                    classId,
+                });
+                getTopicOfClass();
+            }
+        } catch (err) {
+            console.log(err);
+            toast('Create topic failed');
+        }
+        handleCloseModalAddTopic();
+    };
 
-  const getPrerequisiteTopicOfGrade = async () => {
-    // const res = await axiosJWT.get(
-    //   API_URL + `topic/teacher/grade/${currentGrade}`,
-    // )
-    try {
-      const res = await axiosJWT.get(
-        API_URL + `topic/teacher/grade/${watchGrade?.id}`,
-      )
-      setPrerequisiteTopicGrades(res.data)
-    } catch (err) {
-      console.log(err)
-    }
-  }
+    const customStyles = {
+        overlay: {
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: 'rgba(165, 165, 165, 0.6)',
+        },
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            border: 'none',
+            borderRadius: '8px',
+        },
+    };
 
-  useEffect(() => {
-    getPrerequisiteTopicOfGrade()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [watchGrade])
+    let watchGrade = watch('gradeId');
 
-  return (
-    <Modal
-      isOpen={modalTopicIsOpen}
-      style={customStyles}
-      contentLabel="Example Modal"
-      ariaHideApp={false}
-    >
-      <div className="flex justify-end">
-        <button onClick={handleCloseModalCreateTopic}>
-          <i className="fas fa-times"></i>
-        </button>
-      </div>
-      <form
-        className="flex flex-col w-[500px]"
-        onSubmit={handleSubmitCreate(handleCreateTopic)}
-      >
-        <div className="flex flex-col gap-4">
-          <div className="flex justify-center">
-            <h2 className="text-2xl font-semibold">Create new topic</h2>
-          </div>
-          <div className="flex flex-col gap-2">
-            <label htmlFor="topicName">Topic name</label>
-            <input
-              type="text"
-              name="topicName"
-              placeholder="Enter topic name"
-              className="outline-none px-3 py-1 border-b-2  border-opacity-0 transition-all focus:border-primary"
-              {...registerCreate('topicName', { required: true })}
-            />
-          </div>
-          <div className="flex flex-col gap-2">
-            <label htmlFor="description">Topic description</label>
-            <textarea
-              type="text"
-              name="description"
-              placeholder="Description"
-              className="resize-none outline-none px-3 py-1 border-b-2  border-opacity-0 transition-all focus:border-primary"
-              {...registerCreate('description', { required: true })}
-            />
-          </div>
-          <div className="flex flex-row items-center gap-3">
-            <input
-              id="lockTopicCreate"
-              type="checkbox"
-              {...registerCreate('isLock')}
-            />
-            <label
-              htmlFor="lockTopicCreate"
-              className="select-none cursor-pointer"
+    const getPrerequisiteTopicOfGrade = async () => {
+        // const res = await axiosJWT.get(
+        //   API_URL + `topic/teacher/grade/${currentGrade}`,
+        // )
+        try {
+            const res = await axiosJWT.get(API_URL + `topic/teacher/grade/${watchGrade?.id}`);
+            setPrerequisiteTopicGrades(res.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        getPrerequisiteTopicOfGrade();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [watchGrade]);
+
+    return (
+        <Modal
+            isOpen={modalTopicIsOpen}
+            style={customStyles}
+            contentLabel='Example Modal'
+            ariaHideApp={false}
+        >
+            <div className='flex justify-end'>
+                <button onClick={handleCloseModalCreateTopic}>
+                    <i className='fas fa-times'></i>
+                </button>
+            </div>
+            <form
+                className='flex flex-col w-[500px]'
+                onSubmit={handleSubmitCreate(handleCreateTopic)}
             >
-              Lock topic
-            </label>
-          </div>
-          <div className="flex flex-col gap-2">
-            <label htmlFor="gradeId">Grade</label>
-            <Controller
-              name="gradeId"
-              control={control}
-              rules={{ required: true }}
-              render={({ field }) => (
-                <Select
-                  {...field}
-                  onChange={field.onChange}
-                  options={grades}
-                  getOptionValue={(option) => option.id}
-                  getOptionLabel={(option) => option.gradeName}
-                  styles={{
-                    menuList: (base) => ({
-                      ...base,
-                      height: '100px', // your desired height
-                    }),
-                  }}
-                ></Select>
-              )}
-            />
-          </div>
+                <div className='flex flex-col gap-4'>
+                    <div className='flex justify-center'>
+                        <h2 className='text-2xl font-semibold'>Create new topic</h2>
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                        <label htmlFor='topicName'>Topic name</label>
+                        <input
+                            type='text'
+                            name='topicName'
+                            placeholder='Enter topic name'
+                            className='outline-none px-3 py-1 border-b-2  border-opacity-0 transition-all focus:border-primary'
+                            {...registerCreate('topicName', { required: true })}
+                        />
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                        <label htmlFor='description'>Topic description</label>
+                        <textarea
+                            type='text'
+                            name='description'
+                            placeholder='Description'
+                            className='resize-none outline-none px-3 py-1 border-b-2  border-opacity-0 transition-all focus:border-primary'
+                            {...registerCreate('description', { required: true })}
+                        />
+                    </div>
+                    <div className='flex flex-row items-center gap-3'>
+                        <input id='lockTopicCreate' type='checkbox' {...registerCreate('isLock')} />
+                        <label htmlFor='lockTopicCreate' className='select-none cursor-pointer'>
+                            Lock topic
+                        </label>
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                        <label htmlFor='gradeId'>Grade</label>
+                        <Controller
+                            name='gradeId'
+                            control={control}
+                            rules={{ required: true }}
+                            render={({ field }) => (
+                                <Select
+                                    {...field}
+                                    onChange={field.onChange}
+                                    options={grades}
+                                    getOptionValue={(option) => option.id}
+                                    getOptionLabel={(option) => option.gradeName}
+                                    styles={{
+                                        menuList: (base) => ({
+                                            ...base,
+                                            height: '100px', // your desired height
+                                        }),
+                                    }}
+                                ></Select>
+                            )}
+                        />
+                    </div>
 
-          <div className="flex flex-col gap-2">
-            <label htmlFor="prerequisiteTopicId">Prerequisite topic</label>
-            {prerequisiteTopicGrades?.length > 0 && (
-              <Controller
-                name="prerequisiteTopicId"
-                control={control}
-                render={({ field }) => (
-                  <Select
-                    {...field}
-                    options={prerequisiteTopicGrades}
-                    getOptionValue={(option) => option.id}
-                    getOptionLabel={(option) => option.topicName}
-                    styles={{
-                      menuList: (base) => ({
-                        ...base,
-                        height: '200px', // your desired height
-                      }),
-                    }}
-                    menuPlacement="top"
-                  ></Select>
-                )}
-              ></Controller>
-            )}
-          </div>
-        </div>
-        <Button className="border-none bg-primary w-full mt-5">Create</Button>
-      </form>
-    </Modal>
-  )
-}
+                    <div className='flex flex-col gap-2'>
+                        <label htmlFor='prerequisiteTopicId'>Prerequisite topic</label>
+                        {prerequisiteTopicGrades?.length > 0 && (
+                            <Controller
+                                name='prerequisiteTopicId'
+                                control={control}
+                                render={({ field }) => (
+                                    <Select
+                                        {...field}
+                                        options={prerequisiteTopicGrades}
+                                        getOptionValue={(option) => option.id}
+                                        getOptionLabel={(option) => option.topicName}
+                                        styles={{
+                                            menuList: (base) => ({
+                                                ...base,
+                                                height: '200px', // your desired height
+                                            }),
+                                        }}
+                                        menuPlacement='top'
+                                    ></Select>
+                                )}
+                            ></Controller>
+                        )}
+                    </div>
+                </div>
+                <Button className='border-none bg-primary w-full mt-5'>Create</Button>
+            </form>
+        </Modal>
+    );
+};
 
-export default ModalCreateTopic
+export default ModalCreateTopic;
