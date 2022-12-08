ada1115f4e70fd4a9285aa8530d997e5fe5de225
Tien Dang <tien140801@gmail.com>
2022-12-09 00:06:53 +0700

    Update to host

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,2 @@
 export const API_URL = 'http://localhost:8080/api/';
+// export const API_URL = 'http://54.160.194.254:8080/api/';
--- src/pages/AdminPages/ManageAccount.js
+++ src/pages/AdminPages/ManageAccount.js
@@ -1,104 +1,113 @@
-import React, { useState } from 'react'
-import Button from '../../components/Button'
-import ManageAccountModal from '../../components/Modals/ManageAccountModal'
+import React, { useState, useEffect } from 'react';
+import Button from '../../components/Button';
+import ManageAccountModal from '../../components/Modals/ManageAccountModal';
+import { API_URL } from '../../constant';
+import createAxiosJWT from '../../createAxiosJWT';
+import TokenExpire from '../../components/Modals/TokenExpire';
 
+const axiosJWT = createAxiosJWT();
 const ManageAccount = () => {
-  const [openManageAccount, setOpenManageAccount] = useState(false)
+    const [openManageAccount, setOpenManageAccount] = useState(false);
+    const [isExpired, setIsExpired] = useState(false);
 
-  return (
-    <div className="px-10 py-7 flex flex-col gap-5">
-      <div className="flex flex-row items-center justify-between">
-        <span className="text-2xl font-[500]">Manage Account</span>
-        <Button
-          onClick={() => {
-            setOpenManageAccount(true)
-          }}
-        >
-          Add an account
-        </Button>
-        <ManageAccountModal
-          isOpen={openManageAccount}
-          setIsOpen={setOpenManageAccount}
-        />
-      </div>
-      <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className=" text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="py-3 px-6">
-                Id
-              </th>
-              <th scope="col" className="py-3 px-6">
-                Email
-              </th>
-              <th scope="col" className="py-3 px-6">
-                Name
-              </th>
-              <th scope="col" className="py-3 px-6">
-                Active
-              </th>
-              <th scope="col" className="py-3 px-6">
-                Role
-              </th>
-              <th scope="col" className="py-3 px-6">
-                <span className="sr-only">Edit</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <th scope="row" className="py-4 px-6">
-                1
-              </th>
-              <th className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Apple MacBook Pro 17"
-              </th>
-              <td className="py-4 px-6">Sliver</td>
-              <td className="py-4 px-6">Laptop</td>
-              <td className="py-4 px-6">$2999</td>
-              <td className="py-4 px-6 text-right">
-                <span className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                  Edit
-                </span>
-              </td>
-            </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <th scope="row" className="py-4 px-6">
-                1
-              </th>
-              <th className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Apple MacBook Pro 17"
-              </th>
-              <td className="py-4 px-6">Sliver</td>
-              <td className="py-4 px-6">Laptop</td>
-              <td className="py-4 px-6">$2999</td>
-              <td className="py-4 px-6 text-right">
-                <span className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                  Edit
-                </span>
-              </td>
-            </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <th scope="row" className="py-4 px-6">
-                1
-              </th>
-              <th className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Apple MacBook Pro 17"
-              </th>
-              <td className="py-4 px-6">Sliver</td>
-              <td className="py-4 px-6">Laptop</td>
-              <td className="py-4 px-6">$2999</td>
-              <td className="py-4 px-6 text-right">
-                <span className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                  Edit
-                </span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  )
-}
+    useEffect(() => {
+        const getAllAccounts = async () => {
+          
+        };
+        getAllAccounts();
+    }, []);
+    return (
+        <div className='px-10 py-7 flex flex-col gap-5'>
+            <div className='flex flex-row items-center justify-between'>
+                <span className='text-2xl font-[500]'>Manage Account</span>
+                <Button
+                    onClick={() => {
+                        setOpenManageAccount(true);
+                    }}
+                >
+                    Add an account
+                </Button>
+                <ManageAccountModal isOpen={openManageAccount} setIsOpen={setOpenManageAccount} />
+            </div>
+            <div className='overflow-x-auto relative shadow-md sm:rounded-lg'>
+                <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
+                    <thead className=' text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+                        <tr>
+                            <th scope='col' className='py-3 px-6'>
+                                Id
+                            </th>
+                            <th scope='col' className='py-3 px-6'>
+                                Email
+                            </th>
+                            <th scope='col' className='py-3 px-6'>
+                                Name
+                            </th>
+                            <th scope='col' className='py-3 px-6'>
+                                Active
+                            </th>
+                            <th scope='col' className='py-3 px-6'>
+                                Role
+                            </th>
+                            <th scope='col' className='py-3 px-6'>
+                                <span className='sr-only'>Edit</span>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'>
+                            <th scope='row' className='py-4 px-6'>
+                                1
+                            </th>
+                            <th className='py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td className='py-4 px-6'>Sliver</td>
+                            <td className='py-4 px-6'>Laptop</td>
+                            <td className='py-4 px-6'>$2999</td>
+                            <td className='py-4 px-6 text-right'>
+                                <span className='font-medium text-blue-600 dark:text-blue-500 hover:underline'>
+                                    Edit
+                                </span>
+                            </td>
+                        </tr>
+                        <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'>
+                            <th scope='row' className='py-4 px-6'>
+                                1
+                            </th>
+                            <th className='py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td className='py-4 px-6'>Sliver</td>
+                            <td className='py-4 px-6'>Laptop</td>
+                            <td className='py-4 px-6'>$2999</td>
+                            <td className='py-4 px-6 text-right'>
+                                <span className='font-medium text-blue-600 dark:text-blue-500 hover:underline'>
+                                    Edit
+                                </span>
+                            </td>
+                        </tr>
+                        <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'>
+                            <th scope='row' className='py-4 px-6'>
+                                1
+                            </th>
+                            <th className='py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td className='py-4 px-6'>Sliver</td>
+                            <td className='py-4 px-6'>Laptop</td>
+                            <td className='py-4 px-6'>$2999</td>
+                            <td className='py-4 px-6 text-right'>
+                                <span className='font-medium text-blue-600 dark:text-blue-500 hover:underline'>
+                                    Edit
+                                </span>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <TokenExpire isOpen={isExpired} />
+        </div>
+    );
+};
 
-export default ManageAccount
+export default ManageAccount;
