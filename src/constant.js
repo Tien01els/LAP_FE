0fac9318411e00bbb336fded4874d21e4d14ceba
Tien Dang <tien140801@gmail.com>
2022-12-18 11:49:03 +0700

    Update login UI

--- src/constant.js
+++ src/constant.js
@@ -1,26 +1,26 @@
-export const API_URL = 'http://localhost:8080/api/'
-export const SOCKET_API_URL = 'http://localhost:8080/'
-// export const API_URL = 'http://54.160.194.254:8080/api/';
-// export const SOCKET_API_URL = 'http://54.160.194.254:8080/';
+export const API_URL = 'http://localhost:8080/api/';
+export const SOCKET_API_URL = 'http://localhost:8080/';
+// export const API_URL = 'http://3.208.8.67:8080/api/';
+// export const SOCKET_API_URL = 'http://3.208.8.67:8080/';
 
 export const customStyles = {
-  overlay: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: 'rgba(165, 165, 165, 0.6)',
-  },
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    border: 'none',
-    borderRadius: '8px',
-    overflow: 'visible',
-  },
-}
+    overlay: {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(165, 165, 165, 0.6)',
+    },
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        border: 'none',
+        borderRadius: '8px',
+        overflow: 'visible',
+    },
+};
